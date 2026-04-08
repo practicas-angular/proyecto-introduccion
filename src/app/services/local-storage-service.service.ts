1cb67187d833467980a74b593b0aea70b09d2f5e
gabino <jose.murielsanchez@plexus.es>
2026-04-08 11:35:54 +0200

    🔥FIX_vercel_animation import

--- src/app/services/local-storage-service.service.ts
+++ src/app/services/local-storage-service.service.ts
@@ -1,9 +1,7 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class LocalStorageService {
   private isBrowser: boolean;
 
