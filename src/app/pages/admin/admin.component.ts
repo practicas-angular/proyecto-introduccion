22ff0928b613b9c26437bdf807ff56d5b4f8fc54
gabino <jose.murielsanchez@plexus.es>
2026-04-09 11:34:56 +0200

    ✅NEW_pipes simples

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import {
+  CurrencyPipe,
+  DatePipe,
+  JsonPipe,
+  LowerCasePipe,
+  PercentPipe,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
 
 @Component({
   selector: 'app-admin',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    UpperCasePipe,
+    LowerCasePipe,
+    TitleCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    PercentPipe,
+    SlicePipe,
+    JsonPipe,
+  ],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+  styleUrl: './admin.component.scss',
 })
 export class AdminComponent {
-
+  today = new Date();
+  userObject = {
+    id: 1,
+    nombre: 'Juan',
+    email: 'juan@mail.com',
+    roles: ['admin', 'editor'],
+  };
 }
